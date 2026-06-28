92a51dc7567d335c566a6e53f1b51a043c81cbcd
potatmen <ashatokhi2-c@my.cityu.edu.hk>
2026-06-28 17:30:49 +0100

    add all stacks to bench

--- tests/bench.cpp
+++ tests/bench.cpp
@@ -1,46 +1,64 @@
 #include "../blocking_stack.hpp"
-#include "../lock_free_stack_seq_cst.hpp"
+#include "../lf_leaking_seq_cst.hpp"
+#include "../lf_leaking_acq_rel.hpp"
+#include "../lf_reclaim_seq_cst.hpp"
+#include "../lf_reclaim_acq_rel.hpp"
+#include "../lf_reclaim_ar_padded.hpp"
 #include <benchmark/benchmark.h>
 #include <thread>
 
-constexpr size_t size = 1e5;
-
-static void BM_LockFree_Single(benchmark::State &state) {
-  LockFreeStack<int> s;
-  for (auto _ : state) {
-    s.push(42);
-    benchmark::DoNotOptimize(s.pop());
-  }
-}
-BENCHMARK(BM_LockFree_Single);
-
-static void BM_Blocking_Single(benchmark::State &state) {
-  BlockingStack<int> s(size);
-  for (auto _ : state) {
-    s.push(42);
-    benchmark::DoNotOptimize(s.pop());
-  }
-}
-BENCHMARK(BM_Blocking_Single);
-
-static void BM_LockFree_Concurrent(benchmark::State &state) {
-  static LockFreeStack<int> s;
+static void BM_Leaking_SC(benchmark::State &state) {
+  static leaking_sc::LockFreeStack<int> s;
   for (auto _ : state) {
     s.push(state.thread_index());
     benchmark::DoNotOptimize(s.pop());
   }
 }
-BENCHMARK(BM_LockFree_Concurrent)
-    ->ThreadRange(1, std::thread::hardware_concurrency());
+BENCHMARK(BM_Leaking_SC)->Threads(1)->Threads(2)->Threads(4)->Threads(8)->Threads(16);
 
-static void BM_Blocking_Concurrent(benchmark::State &state) {
-  static BlockingStack<int> s(size);
+static void BM_Leaking_AR(benchmark::State &state) {
+  static leaking_ar::LockFreeStack<int> s;
   for (auto _ : state) {
     s.push(state.thread_index());
     benchmark::DoNotOptimize(s.pop());
   }
 }
-BENCHMARK(BM_Blocking_Concurrent)
-    ->ThreadRange(1, std::thread::hardware_concurrency());
+BENCHMARK(BM_Leaking_AR)->Threads(1)->Threads(2)->Threads(4)->Threads(8)->Threads(16);
+
+static void BM_Reclaim_SC(benchmark::State &state) {
+  static reclaim_sc::LockFreeStack<int> s;
+  for (auto _ : state) {
+    s.push(state.thread_index());
+    benchmark::DoNotOptimize(s.pop());
+  }
+}
+BENCHMARK(BM_Reclaim_SC)->Threads(1)->Threads(2)->Threads(4)->Threads(8)->Threads(16);
+
+static void BM_Reclaim_AR(benchmark::State &state) {
+  static reclaim_ar::LockFreeStack<int> s;
+  for (auto _ : state) {
+    s.push(state.thread_index());
+    benchmark::DoNotOptimize(s.pop());
+  }
+}
+BENCHMARK(BM_Reclaim_AR)->Threads(1)->Threads(2)->Threads(4)->Threads(8)->Threads(16);
+
+static void BM_Reclaim_AR_Padded(benchmark::State &state) {
+  static reclaim_ar_padded::LockFreeStack<int> s;
+  for (auto _ : state) {
+    s.push(state.thread_index());
+    benchmark::DoNotOptimize(s.pop());
+  }
+}
+BENCHMARK(BM_Reclaim_AR_Padded)->Threads(1)->Threads(2)->Threads(4)->Threads(8)->Threads(16);
+
+static void BM_Blocking(benchmark::State &state) {
+  static BlockingStack<int> s;
+  for (auto _ : state) {
+    s.push(state.thread_index());
+    benchmark::DoNotOptimize(s.pop());
+  }
+}
+BENCHMARK(BM_Blocking)->Threads(1)->Threads(2)->Threads(4)->Threads(8)->Threads(16);
 
 BENCHMARK_MAIN();
